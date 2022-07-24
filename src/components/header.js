0a0cb0b197e6760b1c833be2840942f77843f78e
halfcyan <nicholasreed04@gmail.com>
2022-07-24 00:00:03 -0500

    MAJOR UPDATE

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+
+const Header = (_Header) => (
+  <header>
+    <h1 className="text-3xl left-0 p-3 flex float-left absolute text-white">
+      My First Website
+    </h1>
+    <a href="./blog" className="text-xl justify-center object-center flex p-3">
+      Blog
+    </a>
+  </header>
+);
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+};
+
+Header.defaultProps = {
+  siteTitle: ``,
+};
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,89 +1,24 @@
-import * as React from "react"
-import { Link, graphql } from "gatsby"
+import * as React from "react";
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout-index";
+import Seo from "../components/seo";
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata?.title || `Title`
-  const posts = data.allMarkdownRemark.nodes
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <div>
+      <h1 className="text-4xl flex justify-center text-purple-400">hello</h1>
+    </div>
+    <div>
+      <p className="justify-left flex-auto flex-wrap">
+        This is my first attempt at creating a website.
+      </p>
+      <p className="justify-left flex-auto flex-wrap">
+        If you've stuck around long enough to read this, you might as well check
+        out my <a href="./blog">blog.</a> Thanks!
+      </p>
+    </div>
+  </Layout>
+);
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <Seo title="All posts" />
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
-
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
-    </Layout>
-  )
-}
-
-export default BlogIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-        }
-      }
-    }
-  }
-`
+export default IndexPage;
