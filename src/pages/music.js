0be9cd06a9b4f77ed549cd35f859d1849076de75
halfcyan <nicholasreed04@gmail.com>
2022-07-26 14:10:21 -0500

    I honestly don't even know anymore

--- src/pages/music.js
+++ src/pages/music.js
@@ -2,21 +2,30 @@ import * as React from "react";
 
 import Layout from "../components/layout-index";
 import Seo from "../components/seo";
+import musicID from "../../content/music/tidal-links"
+
+let musicElements = "";
+
+musicID.forEach(element => 
+    musicElements = musicElements + `<iframe width="100%" 
+    height="150" 
+    src="https://odesli.co/embed/?url=https%3A%2F%2Fsong.link%2Ft%{element}&theme=dark"
+    frameborder="0" 
+    allowfullscreen 
+    sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox" 
+    allow="clipboard-read; clipboard-write"></iframe>`
+);
 
 const musicPage = () => (
-<Layout>
-    <Seo title="Music" />
-    <div>
-        <iframe 
-            width="100%" 
-            height="150" 
-            src="https://odesli.co/embed/?url=https%3A%2F%2Fsong.link%2Ft%2F72692903&theme=dark" 
-            frameborder="0" 
-            allowfullscreen 
-            sandbox="allow-same-origin allow-scripts allow-presentation allow-popups allow-popups-to-escape-sandbox" 
-            allow="clipboard-read; clipboard-write">Perfect Run by Crooked Colours</iframe>
-    </div>
-</Layout>
+    <Layout>
+        <Seo title="Music" />
+        <div>
+            <h1 className="text-4xl flex justify-center text-purple-400">This is where I spam music and hope it shows up.</h1>
+        </div>
+        <div>
+            {musicElements}
+        </div>
+    </Layout>
 );
 
 export default musicPage
