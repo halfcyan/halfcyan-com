a99b2e2e3ad5d08438540316709432435a5243fe
halfcyan <nicholasreed04@gmail.com>
2022-08-01 14:09:13 -0500

    fix typescript

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import Navbar from "./navbar"
 
-const Header = _Header => (
+const Header = (_Header: any) => (
   <header>
     <a
       className="text-3xl left-0 p-3 flex float-left absolute text-white hover:text-purple-400 transition-colors no-underline font-['Oxygen']"
